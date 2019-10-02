1e4f9c82caf0db7669550673263ad2e1eb9da100
duyanming <dym880@163.com>
2019-10-02 12:22:27 +0800

    升级框架

--- YY.Server/Program.cs
+++ YY.Server/Program.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Linq;
 using System.Threading;
+using Autofac;
 
 namespace YY.Server
 {
+    using dym.EngineData;
+    using dym.Loader;
     using dym.Rpc.Server;
     static class Program
     {
         static void Main(string[] args)
         {
-            Bootstrap.StartUp(args);
+            Bootstrap.StartUp(args,()=> {
+                //dym.Const.SettingService.TraceOnOff = true;
+                var autofac = IocLoader.GetAutoFacContainerBuilder();
+                autofac.RegisterType(typeof(RpcConnectorImpl)).As(typeof(IRpcConnector)).SingleInstance();
+            });
         }
     }
 }
